af481f05b6082170dca598b652a10bef8bb28729
Refilwe mohlala <refilwemohlalaa@gmail.com>
2025-03-15 15:09:59 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "eks_vpc"
+  }
+}
+
+# Subnet A in Availability Zone us-east-1a
+resource "aws_subnet" "eks_subnet_a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "eks_subnet_a"
+  }
+}
+
+# Subnet B in Availability Zone us-east-1b
+resource "aws_subnet" "eks_subnet_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "eks_subnet_b"
+  }
+}
+
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_role" {
+  name = "eks_cluster_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = { Service = "eks.amazonaws.com" },
+      Effect = "Allow"
+    }]
+  })
+}
+
+# EKS Policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "demo-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_a.id,
+      aws_subnet.eks_subnet_b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+}
